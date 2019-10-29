4472442fc85397f449047cadf234cfde6093ad45
kpoe03 <52971991+kpoe03@users.noreply.github.com>
2019-10-28 22:22:11 -0400

    added pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card'
-import PaginationNav from './components/PaginationNav';
+import PaginationFooter from './components/PaginationFooter';
 
 class App extends Component {
   constructor() {
@@ -39,7 +39,7 @@ class App extends Component {
       <div className="App">
         <h1 className="Header">React Wars</h1>
         <Card characters={this.state.starwarsChars}/>
-        <PaginationNav />
+        <PaginationFooter />
       </div>
     );
   }
